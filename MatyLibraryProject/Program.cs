30c1325f980797bdc9c84a66bf2c125404590950
hayden brown <haydenbrowncinci@gmail.com>
2022-02-08 15:54:03 -0500

    Add project files.

---
+++ MatyLibraryProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MatyLibraryProject {
+    class Program {
+        static void Main(string[] args) {
+            var a = Math.Add("1", "2"); //var changes the variable based on the parameter type  
+        }                                 //(this is Polomorphism) hover over 'a' to see it change 
+    }
+}
